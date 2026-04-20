ce3f61550dfa6cfaf41e18064cba19665d82c4ff
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2026-04-20 14:24:12 -0300

    test: add application context test

---
+++ src/test/java/com/example/gestao_vaga/GestaoVagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestao_vaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
