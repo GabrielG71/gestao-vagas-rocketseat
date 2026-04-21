c02383fa6fab4fd3140dd9075e6940456dd4dda8
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2026-04-21 15:47:23 -0300

    feat: persist candidate and validate duplicates

--- src/main/java/com/example/gestao_vaga/modules/candidates/controllers/CandidateController.java
+++ src/main/java/com/example/gestao_vaga/modules/candidates/controllers/CandidateController.java
@@ -1,11 +1,14 @@
 package com.example.gestao_vaga.modules.candidates.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.gestao_vaga.exceptions.UserFoundExpection;
 import com.example.gestao_vaga.modules.candidates.CandidateEntity;
+import com.example.gestao_vaga.modules.candidates.CandidateRepository;
 
 import jakarta.validation.Valid;
 
@@ -13,9 +16,18 @@ import jakarta.validation.Valid;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create( @Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create( @Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+        .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+        .ifPresent((user) -> {
+            throw new UserFoundExpection();
+        });
+
+        return this.candidateRepository.save(candidateEntity);
     }
 
 }
