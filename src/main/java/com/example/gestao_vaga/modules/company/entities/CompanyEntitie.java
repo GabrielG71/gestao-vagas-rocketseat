2b1bf76e80fabdc9ad2005e3274cb28138b5170e
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2026-04-21 22:48:08 -0300

    feat: create company entity

---
+++ src/main/java/com/example/gestao_vaga/modules/company/entities/CompanyEntitie.java
@@ -0,0 +1,43 @@
+package com.example.gestao_vaga.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity(name = "company")
+public class CompanyEntitie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não pode ter espaços")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 10, max = 100, message = "O campoo [senha] deve conter entre 10 e 100 caracteres.")
+    private String password;
+
+    private String website;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
